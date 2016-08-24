29c452fdfcc2107763537fbc93a146e8cd315aa0
Tomas Korcak <korczis@gmail.com>
2016-08-24 12:45:37 +0200

    Update config template

--- config.template.rb
+++ config.template.rb
@@ -19,7 +19,7 @@ $CONFIG = {
     query: {
       provisioning: 'SELECT identifier as client_id, name as project_title, segment as segment_id FROM lcm_projects LIMIT 1;',
       domain_users: 'SELECT distinct LOWER(login) as login, LOWER(login) as email, first_name, last_name FROM lcm_users;',
-      project_users: 'SELECT client_id as custom_project_id, distinct LOWER(login) as login, role FROM lcm_users;'
+      project_users: 'SELECT client_id as custom_project_id, LOWER(login) as login, role FROM lcm_users;'
     }
   },
 
