64771d47bafd942291c6d3ab4beb5badd57f4ff3
Tomas Korcak <korczis@gmail.com>
2016-09-21 12:56:16 +0200

    Update config template

--- config.template.rb
+++ config.template.rb
@@ -1,28 +1,36 @@
 $CONFIG = {
   appstore: '${PUBLIC_APPSTORE}:branch/tma:/apps',
-
   username: '<USERNAME>',
   password: '<PASSWORD>',
   domain: '<DOMAIN>',
-  hostname: '<HOSTNAME>',
-  server: '<SERVER>',
-  token: '<TOKEN>',
+  hostname: 'staging.intgdc.com',
+  server: 'https://staging.intgdc.com',
+
+  tokens: {
+    postgres: '<POSTGRES_TOKEN>',
+    vertica: '<VERTICA_TOKEN>',
+  },
 
   segments: {
-    'basic' => '<MASTER_PID>',
-    'premium' => '<MASTER_PID>'
+    basic: '<BASIC_MASTER_PID>',
+    premium: '<PREMIUM_MASTER_PID>'
+  },
+
+  projects: {
+    development: '<DEVELOPMENT_PID>',
+    service: '<SERVICE_PID>'
   },
 
   # ADS Settings
   ads: {
-    username: '<USERNAME>',
-    password: '<PASSWORD>',
+    username: '<SECURE_USERNAME>',
+    password: '<SECURE_PASSWORD>',
     id: '<INSTANCE_ID>',
     query: {
-      provisioning: 'SELECT identifier as client_id, name as project_title, segment as segment_id FROM lcm_projects LIMIT 1;',
+      provisioning: 'SELECT identifier as client_id, name as project_title, segment as segment_id FROM lcm_project LIMIT 1;',
       domain_users: 'SELECT distinct LOWER(login) as login, LOWER(login) as email, first_name, last_name FROM lcm_users;',
-      project_users: 'SELECT client_id as custom_project_id, LOWER(login) as login, role FROM lcm_users;',
-      release: 'SELECT * from lcm_release'
+      project_users: 'SELECT client_id as custom_project_id, LOWER(login) as login, role FROM lcm_user;',
+      release: 'SELECT segment_id, master_project_pid as master_project_id from lcm_release;'
     }
   },
 
@@ -30,9 +38,11 @@ $CONFIG = {
   deploy: {
     username: '<USERNAME>',
     password: '<PASSWORD>',
-    server: '<SERVER>'
+    domain: '<DOMAIN>',
+    server: 'https://staging.intgdc.com'
   },
 
+  # Ignore list for user brick
   whitelist: [
     '@gooddata.com'
   ]
